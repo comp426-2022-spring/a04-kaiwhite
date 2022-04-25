cb410e3472e74cf6ec677b8b970e417825443da8
Kai White <kaiwhite@live.unc.edu>
2022-04-24 22:07:17 -0400

    im trying my best please work

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-// Require Express.js
-const express = require('express')
-const { count } = require('yargs')
-const app = express()
+const fs = require('fs')
+const morgan = require('morgan')
 
+const express = require('express')
+
+const app = express()
 const args = require('minimist')(process.argv.slice(2))
-args["port"]
-var port = args.port || 5000 || process.env.PORT
+
+// If --help, echo help text and exit
+if (args.help || args.h) {
+    console.log(`
+    server.js [options]
+    --port, -p	Set the port number for the server to listen on. Must be an integer
+                between 1 and 65535.
+    --debug, -d If set to true, creates endlpoints /app/log/access/ which returns
+                a JSON access log from the database and /app/error which throws 
+                an error with the message "Error test successful." Defaults to 
+                false.
+    --log		If set to false, no log files are written. Defaults to true.
+                Logs are always written to database.
+    --help, -h	Return this message and exit.
+    `)
+    process.exit(0)
+}
+
+const db = require('./database.js')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const port = args.port ||  5555
 
 /** Coin flip functions 
  * This module will emulate a coin flip given various conditions as parameters as defined below
@@ -102,41 +125,50 @@ var port = args.port || 5000 || process.env.PORT
 
 // Start an app server
 const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',port))
+  console.log(`App is running on port ${port}`)
 });
 
+if (args.log == 'false') {
+  console.log("Nothing")
 
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.statusCode = 200;
-    // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
-    });
-// Default response for any other request
+} else {
+  const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
+  app.use(morgan('combined', { stream: accessLog }))
+}
 
-//single flip
-app.get('/app/flip', (req, res) => {
-    res.status(200).json({"flip" : coinFlip()})
+app.use((req, res, next) => {
+  let logdata = {
+      remoteaddr: req.ip,
+      remoteuser: req.user,
+      time: Date.now(),
+      method: req.method,
+      url: req.url,
+      protocol: req.protocol,
+      httpversion: req.httpVersion,
+      status: res.statusCode,
+      referrer: req.headers['referer'],
+      useragent: req.headers['user-agent']
+  };
+  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
+  next();
+})
+app.get("/app", (req, res, next) => {
+res.status(200).send("API test");
 });
 
-//many flip
-app.get('/app/flips/:number', (req, res) => {
-	let flips = coinFlips(req.params.number);
-  let final = countFlips(flips)
-	res.status(200).json({'raw' : flips, 'summary' : final});
-});
+if (args.debug || args.d) {
+  app.get('/app/log/access/', (req, res, next) => {
+      const stmt = db.prepare("SELECT * FROM accesslog").all();
+    res.status(200).json(stmt);
+  })
 
-//guess flip
-app.get('/app/flip/call/tails', (req, res) => {
-    res.status(200).json(flipACoin("tails"))
-});
+  app.get('/app/error/', (req, res, next) => {
+      throw new Error('Error test')
+  })
+}
 
-app.get('/app/flip/call/heads', (req, res) => {
-    res.status(200).json(flipACoin("heads"))
-});
-
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-});
+app.use(function(req, res) {
+  res.status(404).send("404 NOT FOUND")
+  res.type("text/plain")  
+})
